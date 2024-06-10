f7feec30c8e82eaf4aff6633aba999cb1b6185b5
Joao Pedro Sehnem dos Santos <151693736+joaosehn2406@users.noreply.github.com>
2024-06-10 16:41:32 -0300

    Update Trabalho.java

--- Trabalho.java
+++ Trabalho.java
@@ -1,5 +1,6 @@
 package TrabalhoFinal;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Trabalho {
@@ -7,26 +8,44 @@ public class Trabalho {
     public Trabalho(){
         Scanner in = new Scanner(System.in);
 
-        String tabuleiroJogador[][] = new String[8][8];
-        String tabuleiro[][] = new String[8][8];
+        String tabuleiroJogador[][] = new String[8][8]; // vai aparecer no terminal 
+        String tabuleiro[][] = new String[8][8];  
 
-
-
-        
         in.close();
     }
 
 
-    public void adicionarAgua(String tabuleiro[][], String tabuleiroJogador[][]) {
+    public void inicializacaoTabuleiro(String tabuleiro[][], String tabuleiroJogador[][]) {  // vai preencher tudo com agua 
         for(int l = 0; l < tabuleiro.length; l++ ) {
             for(int c = 0; c < tabuleiro[l].length; c++) {
-                tabuleiro[l][c] = " ~ ";
-                tabuleiroJogador[l][c] = " ~ ";
+                tabuleiro[l][c] = "~ ";
+                tabuleiroJogador[l][c] = "~ ";
             }
         }
     }
 
-    public void imprimirTabuleiro(String matriz[][]){
+    public void posicaoNavios(String tabuleiro[][]) {
+        int contadorNavios = 0;
+        Random random = new Random();
+
+        while(contadorNavios != 10) {
+            int linha = random.nextInt(9) + 0; // 8
+            int coluna = random.nextInt(9) + 0; // 4
+            
+            tabuleiro[linha][coluna] = "N"; // tabuleiro
+
+            if(tabuleiro[linha][coluna] != "N") {
+                contadorNavios++;
+
+            }
+        }
+    }
+
+        
+
+    
+
+    /*public void imprimirTabuleiro(String matriz[][]){
         for(int i = 0; i < matriz.length; i++){
             for(int j = 0; j < matriz[i].length; j++){
                 System.out.print(matriz[i][j] + " ");
@@ -34,10 +53,9 @@ public class Trabalho {
             System.out.println();
         }
     }
+        */
 
-    public void escolherAleatorio(){
 
-    }
 
     
     
