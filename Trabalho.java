7ef996b79ece2812f40daaf88751db164a4144ec
Joao Pedro Sehnem dos Santos <151693736+joaosehn2406@users.noreply.github.com>
2024-06-10 10:25:05 -0300

    Add files via upload

---
+++ Trabalho.java
@@ -0,0 +1,23 @@
+package TrabalhoFinal;
+
+import java.util.Scanner;
+
+public class Trabalho {
+    
+    public Trabalho(){
+
+        Scanner in = new Scanner(System.in);
+
+        int matriz[][] = new int[8][8];
+
+
+        in.close();
+    }
+
+
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
