9c2ec68599156b05554e87b86b6dc9046b521fcf
Joao Pedro Sehnem dos Santos <151693736+joaosehn2406@users.noreply.github.com>
2024-06-10 16:28:06 -0300

    Update Trabalho.java

--- Trabalho.java
+++ Trabalho.java
@@ -7,17 +7,21 @@ public class Trabalho {
     public Trabalho(){
         Scanner in = new Scanner(System.in);
 
-        String matriz[][] = new String[8][8];
-        adicionarAgua(matriz);
-        imprimirTabuleiro(matriz);
+        String tabuleiroJogador[][] = new String[8][8];
+        String tabuleiro[][] = new String[8][8];
+
+
+
+        
         in.close();
     }
 
 
-    public void adicionarAgua(String matriz[][]){
-        for(int linha = 0; linha < matriz.length; linha++){
-            for(int coluna = 0; coluna < matriz[linha].length; coluna++){
-                matriz[linha][coluna] = "~";
+    public void adicionarAgua(String tabuleiro[][], String tabuleiroJogador[][]) {
+        for(int l = 0; l < tabuleiro.length; l++ ) {
+            for(int c = 0; c < tabuleiro[l].length; c++) {
+                tabuleiro[l][c] = " ~ ";
+                tabuleiroJogador[l][c] = " ~ ";
             }
         }
     }
