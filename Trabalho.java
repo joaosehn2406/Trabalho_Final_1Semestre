259102e7c204f6b7cd427304314e8d9706452bf9
Joao Pedro Sehnem dos Santos <151693736+joaosehn2406@users.noreply.github.com>
2024-06-11 16:20:21 -0300

    Update Trabalho.java

--- Trabalho.java
+++ Trabalho.java
@@ -12,27 +12,25 @@ public class Trabalho {
         String tabuleiro[][] = new String[8][8];
 
         inicializacaoTabuleiro(tabuleiro, tabuleiroJogador);
-        posicaoNavios(tabuleiro);
-        interacaoJogador(tabuleiro, tabuleiroJogador, in);
-        fimDoJogo(tabuleiro, tabuleiroJogador, 0, 0);
+
+        int resultado = interacaoJogador(tabuleiro, tabuleiroJogador, in);
+        
+        fimDoJogo(tabuleiro, tabuleiroJogador, resultado);
 
         in.close();
     }
 
-    public void inicializacaoTabuleiro(String tabuleiro[][], String tabuleiroJogador[][]) { // vai preencher tudo com
-                                                                                            // agua
+    public void inicializacaoTabuleiro(String tabuleiro[][], String tabuleiroJogador[][]) {                                                          
+        Random random = new Random();
+        
         for (int l = 0; l < tabuleiro.length; l++) {
             for (int c = 0; c < tabuleiro[l].length; c++) {
                 tabuleiro[l][c] = "~";
                 tabuleiroJogador[l][c] = "~";
             }
         }
-    }
-
-    public void posicaoNavios(String tabuleiro[][]) {
         int contadorNavios = 0;
-        Random random = new Random();
-
+    
         while (contadorNavios != 10) {
             int linha = random.nextInt(8);
             int coluna = random.nextInt(8);
@@ -44,31 +42,33 @@ public class Trabalho {
         }
     }
 
-    public void interacaoJogador(String tabuleiro[][], String tabuleiroJogador[][], Scanner in) {
+    public int interacaoJogador(String tabuleiro[][], String tabuleiroJogador[][], Scanner in) {
         int jogadas = 0; // acaba com 30
         int naviosDestruidos = 0; // acaba com 10
 
         while (jogadas < 30 && naviosDestruidos < 10) {
 
-            System.out.println("Digite as coordenadas para atacar(linha e coluna):");
+            System.out.println("Informe uma linha para o ataque e uma coluna para o ataque");
             int x = in.nextInt(); // linha
             int y = in.nextInt(); // coluna
 
-            if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+            if (x >= 0 && x < 8 && y >= 0 && y < 8) {
                 if (tabuleiroJogador[x][y] == "X" || tabuleiroJogador[x][y] == "O") {
                     System.out.println("Esta casa já foi atacada, insira outra.");
                     continue;
-                }
-
-                if (tabuleiro[x][y].equals("N")) {
-                    tabuleiroJogador[x][y] = "X";
-                    naviosDestruidos++;
-                    System.out.println("Você acertou!");
                 } else {
-                    tabuleiroJogador[x][y] = "O";
-                    System.out.println("Você errou!");
+                    System.out.println("Você acertou!");
+                    jogadas++;
+
+                    if(tabuleiro[x][y].equals("N")) {
+                        tabuleiroJogador[x][y] = "X";
+                        naviosDestruidos++;
+                        System.out.println("Você acertou!");
+                    } else {
+                        tabuleiroJogador[x][y] = "O";
+                        System.out.println("Você errou!");
+                    }
                 }
-                jogadas++;
 
                 for (int linha = 0; linha < tabuleiro.length; linha++) {
                     for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
@@ -77,13 +77,15 @@ public class Trabalho {
                     System.out.println();
                 }
             } else {
-                System.out.println("Coordenadas indisponíveis. Insira valores entre 0 e 7.");
+                System.out.println("Coordenadas indisponíveis. Insira valores entre 0-7.");
                 continue;
             }
         }
+        return naviosDestruidos;
     }
 
-    public void fimDoJogo(String tabuleiro[][], String tabuleiroJogador[][], int naviosDestruidos, int jogadas) {
+    public void fimDoJogo(String tabuleiro[][], String tabuleiroJogador[][], int naviosDestruidos) {
+        System.out.println();
         for (int linha = 0; linha < tabuleiro.length; linha++) {
             for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
                 System.out.print(tabuleiro[linha][coluna] + " ");
